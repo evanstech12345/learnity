f4f1a09c03d6bb5d4397ca3e6b29c2cf4db4ed8b
evanstech12345 <evanshcode@gmail.com>
2021-07-10 14:15:31 -0400

    adding a turn on btn for webcam

---
+++ static/app.js
@@ -0,0 +1,17 @@
+var video = document.querySelector("#videoElement");
+
+camBtn = document.getElementById('faceCamBtn');
+
+
+addEventListener('click', e => {
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+}
+)
