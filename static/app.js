af096d799ed60b49adee8f1578efdae8a325bc75
evanstech12345 <evanshcode@gmail.com>
2021-07-17 23:00:44 -0400

    creating the video screen and making it so you can close it after you open it

--- static/app.js
+++ static/app.js
@@ -1,14 +1,19 @@
-var video = document.querySelector("#videoElement");
 
-var nextSlide = document.getElementById('next_slide');
 
-var helloParagraph = document.getElementById('helloPara');
+let videodiv = document.getElementById('videodiv');
+
+let video = document.querySelector("#videoElement");
+
+let nextSlide = document.getElementById('next_slide');
+
+let helloParagraph = document.getElementById('helloPara');
 
 camBtn = document.getElementById('faceCamBtn');
 
 
-addEventListener('click', e => {
-if (navigator.mediaDevices.getUserMedia) {
+camBtn.addEventListener('click', e => {
+  camBtn.innerText = "Turn Off Video";
+  if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
       video.srcObject = stream;
@@ -16,12 +21,11 @@ if (navigator.mediaDevices.getUserMedia) {
     .catch(function (err0r) {
       console.log("Something went wrong!");
     });
+}  
+if (camBtn.innerText = "Turn Off Video") {
+  camBtn.addEventListener('click', j => {
+    video.style.display = "none";
+  })
 }
 }
 )
-
-
-addEventListener('click', e => {
-  document.getElementById('helloPara').innerHTML = 'how to say hello';
-  document.getElementById('helloPara').innerHTML = 'how to say your welcome';
-});
